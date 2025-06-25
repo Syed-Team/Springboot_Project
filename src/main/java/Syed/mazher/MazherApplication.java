ea48a1416c664aa22137618c3e47dbf0cf1263a1
syed Git <syed@git.com>
2025-06-25 13:54:57 +0530

    Springboot project in render

---
+++ src/main/java/Syed/mazher/MazherApplication.java
@@ -0,0 +1,13 @@
+package Syed.mazher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MazherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MazherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Syed/mazher/MazherApplicationTests.java
@@ -0,0 +1,13 @@
+package Syed.mazher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MazherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
